08b375fa7c026d8f53c9a060acb22a81abcbc608
Juliano Pereira Chagas <chagas.julianopereira@gmail.com>
2026-07-15 12:22:03 -0300

    Arquivos iniciais, aula de herança, continuação, ainda em progesso, a recem no incio

---
+++ src/Aluno.java
@@ -0,0 +1,36 @@
+public class Aluno extends Pessoa {
+
+    private int matricula;
+    private String curso;
+
+    public void cancelarMatricula(){
+        System.out.println("Matricula cancelada");
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + getNome() + '\'' +
+                ", idade=" + getIdade() +
+                ", sexo='" + getSexo() + '\'' +
+                ", matricula=" + matricula +
+                ", curso='" + curso + '\'' +
+                '}';
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,47 @@
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    private String sexo;
+
+    public void fazerAniversario() {
+        this.idade++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = Integer.parseInt(idade);
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", sexo='" + sexo + '\'' +
+                '}';
+    }
+
+    public void receberAum(String s) {
+    }
+}
